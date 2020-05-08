883cd45bb62aee3b28b8ef686d26401f02968383
victorcolome <victorcolomec@gmail.com>
2020-05-08 22:36:28 +0200

    Read dataset

--- sample/__init__.py
+++ sample/__init__.py
@@ -1,2 +1,22 @@
+import pandas as pd
+import numpy as np
+
+# Descripción del dataset en http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.names
+
 if __name__ == '__main__':
-	pass
+    # Read dataset
+    url = "http://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
+    df = pd.read_csv(url, na_values=[' ?'])
+    df.columns = ["age", "workclass", "fnlwgt", "education", "education-num", "marital_status", "occupation",
+                  "relationship", "race", "sex", "capital_gain", "capital_loss", "hour_per_week", "native_country",
+                  "income"]
+    print(df.head())
+
+    # TODO VC: NaN values
+    print(df.isna().sum())
+
+    # TODO VC: Outliers
+
+    # TODO VC: Store data
+
+    # TODO VC: Correlation
